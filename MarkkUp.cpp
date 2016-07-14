b5d0518fe74deb0f3cc17cc8957fc2a0ba3e90dc
sharminnie0786 <sharmynkayani1@gmail.com>
2016-07-14 14:03:48 +1000

    Add files via upload

---
+++ MarkkUp.cpp
@@ -0,0 +1,29 @@
+//
+//  main.cpp
+//  TextBookQuestions
+//
+//  Created by Sharmyn Kayani on 14/07/2016.
+//  Copyright © 2016 Sharmyn Kayani. All rights reserved.
+//
+//  Retail price = wholesale * percentage = wholesale + percentage  value
+
+//#include <iostream>
+//
+//using namespace std;
+//
+//////defined prototypes
+//double  calculateRetail(float, float);
+//
+//int  main() {
+//    cout << "Your retail  price is: $"  << calculateRetail(5, 20) << endl;
+//}
+//
+////will take arguments in
+//double calculateRetail(float wholsale, float markup){
+//    float retailPrice, initial;
+//    
+//    initial =  wholsale * (markup/100);
+//    retailPrice = wholsale +initial;
+//    
+//    return retailPrice;
+//}

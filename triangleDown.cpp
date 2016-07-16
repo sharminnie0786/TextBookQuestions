475294feec5ef6ba235416959e64ad9c7c3a5641
sharminnie0786 <sharmynkayani1@gmail.com>
2016-07-16 12:53:57 +1000

    Add files via upload

---
+++ triangleDown.cpp
@@ -0,0 +1,20 @@
+//
+//  triangleDown.cpp
+//  TextBookQuestions
+//
+//  Created by Sharmyn Kayani on 16/07/2016.
+//  Copyright © 2016 Sharmyn Kayani. All rights reserved.
+//
+//  Logic Question 34.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 0; i < 5; i++) {
+        for  (int j = i; j < 5; j++) {
+            cout <<  "*";
+        }
+        cout << endl;
+    }
+}

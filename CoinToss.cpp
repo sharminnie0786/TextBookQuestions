0fd9e9a0a9df2954ddc8f647df2b15b693015dc8
sonicaj <waqarahmedjoyia@live.com>
2016-07-16 12:17:47 +0500

    Update CoinToss.cpp

--- CoinToss.cpp
+++ CoinToss.cpp
@@ -7,6 +7,9 @@
 //
 //  Write  a function  named  coinToss that  simulates  the tossing  of a coin.  When  you call the function, it should generate a random number  in the range of 1 through 2. If the random number is 1, the function  should  display    heads.    If the random num- ber is 2, the function should  display    tails.   Demonstrate the function  in a program that  asks the user how many times the coin should  be tossed, and then simulates the tossing of the coin that  number  of times.
 
+
+// I have uploaded this code with some corrections in the logic branch
+
 #include <iostream>
 using namespace std;
 
